92f45773ad10d9b7da7b76a06426e80d374b10a7
Higuchi <higuchi@insatsuya-Vostro-1200.(none)>
2012-01-04 17:49:38 +0900

    few change of osext

--- include/yomogi/ostream_ext.hpp
+++ include/yomogi/ostream_ext.hpp
@@ -1,31 +1,49 @@
 #ifndef OSTREAM_EXT_HPP
 #define OSTREAM_EXT_HPP
 
-  template<typename T>
-  std::ostream&
-  ostream_each(
-      std::ostream & os,
-      T val
-      	       )
-  {
-    os << val;
+namespace yomogi{
+  namespace ostream_ext{
+    template <char Separator = ',', typename T>
+    std::ostream&
+    each_output(
+	std::ostream & os,
+	T val
+		)
+    {
+      os << val;
 
-    return os;
-  }
+      return os;
+    }
   
-  
-  template <typename T, typename ...ArgTypes>
-  std::ostream&
-  ostream_each(
-      std::ostream & os,
-      T val,
-      ArgTypes... args
-	       )
-  {
-    os << val;
+    
+    template <char Separator = ',', typename T, typename ...ArgTypes>
+    std::ostream&
+    each_output(
+	std::ostream & os,
+	T val,
+	ArgTypes... args
+		)
+    {
+      os << val << Separator;
+      
+      return each_output<Separator>(os, args...);
+    }
 
-    return ostream_each(os, args...);
+
+    template <char Separator, typename ...ArgTypes>
+    std::ostream&
+    dsv(
+	std::ostream & os,
+	ArgTypes... args
+		)
+    {
+      //os << '';
+      each_output<Separator>(os, args...);
+      //os << ')';
+
+      return os;
+    }
+
   }
-
-
+}
 #endif
